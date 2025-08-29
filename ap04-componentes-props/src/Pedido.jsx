6ceaf94d5bda42b00595a9f2c6aaaf71be314130
Jonas Godoy <jonas.gdiamantinos@gmail.com>
2025-08-29 18:04:12 -0300

    feat(04): alteracoes de estacoes e icones

---
+++ ap04-componentes-props/src/Pedido.jsx
@@ -0,0 +1,13 @@
+const Pedido = (props) => (
+    <div className="d-flex">
+      <div className="d-flex align-items-center">
+        <i className={`fa-solid ${props.icone} fa-2x`}></i>
+      </div>
+      <div className="flex-grow-1 ms-2 border p-1">
+        <h4 className="text-center">{props.titulo}</h4>
+        <p className="text-center">{props.descricao}</p>
+      </div>
+    </div>
+)
+
+export default Pedido
